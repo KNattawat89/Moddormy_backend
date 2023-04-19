c9b47658821f28b8cfc61f5373af99625b14a8be
Eve-nichakon <91275476+nichakontae@users.noreply.github.com>
2023-04-20 00:21:32 +0700

    [UPDATE] change struct of file

--- loaders/mysql/model/files.go
+++ loaders/mysql/model/files.go
@@ -1,8 +1,6 @@
 package model
 
 type File struct {
-	FileID *uint64 `json:"file_id" gorm:"primaryKey" gorm:"AUTO_INCREMENT"`
-	Name   *string `json:"name" gorm:"not null"`
-	Type   *string `json:"type" gorm:"not null"`
-	Data   *byte   `json:"data" gorm:"not null"`
+	FileID   *uint64 `json:"file_id" gorm:"primaryKey" gorm:"AUTO_INCREMENT"`
+	FileName *string `json:"name" gorm:"not null"`
 }
