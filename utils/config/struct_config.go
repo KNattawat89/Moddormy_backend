8937b222d6f57aff7e9eca8e51776578f5b7df7b
Eve-nichakon <91275476+nichakontae@users.noreply.github.com>
2023-04-23 13:19:58 +0700

    [UPDATE] add frontend address and cors

--- utils/config/struct_config.go
+++ utils/config/struct_config.go
@@ -1,9 +1,11 @@
 package config
 
 type config struct {
-	LogLevel     uint32 `yaml:"logLevel"`
-	MySqlDsn     string `yaml:"mySqlDsn"`
-	MySqlMigrate bool   `yaml:"mySqlMigrate"`
-	ServerHeader string `yaml:"serverHeader"`
-	BackAddress  string `yaml:"backAddress"`
+	LogLevel     uint32   `yaml:"logLevel"`
+	MySqlDsn     string   `yaml:"mySqlDsn"`
+	MySqlMigrate bool     `yaml:"mySqlMigrate"`
+	ServerHeader string   `yaml:"serverHeader"`
+	BackAddress  string   `yaml:"backAddress"`
+	FrontAddress string   `yaml:"frontAddress"`
+	Cors         []string `yaml:"cors"`
 }
