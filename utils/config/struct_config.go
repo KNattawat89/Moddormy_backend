374c5ff9aa8be113cd47efd1a63916132a23cd70
Eve-nichakon <91275476+nichakontae@users.noreply.github.com>
2023-04-26 14:49:28 +0700

    Update struct_config.go

--- utils/config/struct_config.go
+++ utils/config/struct_config.go
@@ -1,11 +1,12 @@
 package config
 
 type config struct {
-	Path         string `yaml:"path"`
-	URL          string `yaml:"url"`
-	LogLevel     uint32 `yaml:"logLevel"`
-	MySqlDsn     string `yaml:"mySqlDsn"`
-	MySqlMigrate bool   `yaml:"mySqlMigrate"`
-	ServerHeader string `yaml:"serverHeader"`
-	BackAddress  string `yaml:"backAddress"`
+	Path         string   `yaml:"path"`
+	URL          string   `yaml:"url"`
+	LogLevel     uint32   `yaml:"logLevel"`
+	MySqlDsn     string   `yaml:"mySqlDsn"`
+	MySqlMigrate bool     `yaml:"mySqlMigrate"`
+	ServerHeader string   `yaml:"serverHeader"`
+	BackAddress  string   `yaml:"backAddress"`
+	Cors         []string `yaml:"cors"`
 }
