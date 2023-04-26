85d9e9fb95dabe7a255b2f765222546d909abe69
Eve-nichakon <91275476+nichakontae@users.noreply.github.com>
2023-04-26 15:05:43 +0700

    [UPDATE] edit data model

--- loaders/mysql/model/dorm_images.go
+++ loaders/mysql/model/dorm_images.go
@@ -3,8 +3,8 @@ package model
 import "time"
 
 type DormImage struct {
-	DormId    *uint64    `json:"dorm_id" gorm:"not null"`
+	DormId    *uint64    `json:"dorm_id" gorm:"not null;primaryKey"`
 	Dorm      *Dorm      `json:"dorm" gorm:"foreignKey:DormId;references:Id;not null"`
-	FileName  *string    `json:"file_name" gorm:"not null"`
+	FileName  *string    `json:"file_name" gorm:"not null;primaryKey"`
 	UpdatedAt *time.Time `json:"update_at" gorm:"not null"`
 }
--- loaders/mysql/model/room_images.go
+++ loaders/mysql/model/room_images.go
@@ -3,8 +3,8 @@ package model
 import "time"
 
 type RoomImage struct {
-	RoomId    *uint64    `json:"room_id" gorm:"not null"`
+	RoomId    *uint64    `json:"room_id" gorm:"not null;primaryKey"`
 	Room      *Room      `json:"room" gorm:"foreignKey:RoomId;references:Id;not null"`
-	FileName  *string    `json:"file_name" gorm:"not null"`
+	FileName  *string    `json:"file_name" gorm:"not null;primaryKey"`
 	UpdatedAt *time.Time `json:"update_at" gorm:"not null"`
 }
