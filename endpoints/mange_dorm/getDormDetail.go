954fce0da7c196dbf59a238b631880eff6bc5477
Jakrapat Matures <93482855+Jakrapat-M@users.noreply.github.com>
2023-04-29 19:55:25 +0700

    Update getDormDetail.go

---
+++ endpoints/mange_dorm/getDormDetail.go
@@ -0,0 +1,37 @@
+package mange_dorm
+
+import (
+	"Moddormy_backend/loaders/mysql"
+	"Moddormy_backend/loaders/mysql/model"
+	"Moddormy_backend/types/response"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func GetDormDetail(c *fiber.Ctx) error {
+	dormId := c.Query("dormId")
+	if dormId == "" {
+		return &response.GenericError{
+			Message: "dormId is missing from query parameters",
+			Err:     nil,
+		}
+	}
+
+	var dorm model.Dorm
+
+	if result := mysql.Gorm.Where("dorm_id = ?", dormId).First(&dorm); result.Error != nil {
+		return &response.GenericError{
+			Message: "Unable to get dorm",
+			Err:     result.Error,
+		}
+	}
+
+	if dorm.DormId == "" {
+		return &response.GenericError{
+			Message: "No dorm found for dormId",
+			Err:     nil,
+		}
+	}
+
+	return c.JSON(dorm)
+}
