7fe95cd9e92185968162d4d5334c6c6cd16ff350
Eve-nichakon <91275476+nichakontae@users.noreply.github.com>
2023-05-02 16:59:55 +0700

    [UPDATE] dormsearch payload

--- types/payload/dormSearch.go
+++ types/payload/dormSearch.go
@@ -1,5 +1,11 @@
 package payload
 
 type DormSearch struct {
-	DormName *string `json:"dorm_name`
+	DormId     *uint64  `json:"dorm_id"`
+	DormName   *string  `json:"dorm_name"`
+	CoverImage *string  `json:"cover_image"`
+	Rating     *int     `json:"rating"`
+	MinPrice   *float64 `json:"min_price"`
+	MaxPrice   *float64 `json:"max_price"`
+	//IsFav      *bool   `json:"is_fav"`
 }
